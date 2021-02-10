97975420033113df8aefff3dbd9fbd7b203bf198
Mateusz <mateusz.kukiel@gmail.com>
2021-02-10 22:14:51 +0100

    Added LocationRepository interface and GetLocationsUseCase with test

---
+++ app/src/main/java/com/mateuszkukiel/kursakademiaandroida/features/location/domain/GetLocationsUseCase.kt
@@ -0,0 +1,9 @@
+package com.mateuszkukiel.kursakademiaandroida.features.location.domain
+
+import com.mateuszkukiel.kursakademiaandroida.core.base.UseCase
+import com.mateuszkukiel.kursakademiaandroida.features.location.domain.model.Location
+
+class GetLocationsUseCase(private val locationRepository: LocationRepository) :
+    UseCase<List<Location>, Unit>() {
+    override suspend fun action(params: Unit) = locationRepository.getLocations()
+}
---
+++ app/src/test/java/com/mateuszkukiel/kursakademiaandroida/features/location/domain/GetLocationsUseCaseTest.kt
@@ -0,0 +1,27 @@
+package com.mateuszkukiel.kursakademiaandroida.features.location.domain
+
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.GlobalScope
+import org.junit.jupiter.api.Test
+
+internal class GetLocationsUseCaseTest {
+
+    @Test
+    fun `when use case is invoked than execute get locations method from repository`() {
+        //given
+        val repository = mockk<LocationRepository>()
+        val useCase = GetLocationsUseCase(repository)
+
+        //when
+        useCase(
+            params = Unit,
+            scope = GlobalScope
+        )
+
+        //then
+
+        coVerify { repository.getLocations() }
+
+    }
+}
