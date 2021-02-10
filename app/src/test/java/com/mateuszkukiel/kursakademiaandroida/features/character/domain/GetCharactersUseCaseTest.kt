c9cc60671ce24f877292316eb2fbf44ee8de9126
Mateusz <mateusz.kukiel@gmail.com>
2021-02-10 23:00:39 +0100

    Added CharacterRepository interface and GetCharactersUseCase with test

---
+++ app/src/test/java/com/mateuszkukiel/kursakademiaandroida/features/character/domain/GetCharactersUseCaseTest.kt
@@ -0,0 +1,25 @@
+package com.mateuszkukiel.kursakademiaandroida.features.character.domain
+
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.GlobalScope
+import org.junit.jupiter.api.Test
+
+internal class GetCharactersUseCaseTest {
+    @Test
+    fun `when use case is invoked than execute get characters method from repository`() {
+        // given
+        val repository = mockk<CharacterRepository>()
+        val useCase = GetCharactersUseCase(repository)
+
+        // when
+        useCase(
+            params = Unit,
+            scope = GlobalScope
+        )
+
+        // then
+        coVerify { repository.getCharacters() }
+    }
+
+}
